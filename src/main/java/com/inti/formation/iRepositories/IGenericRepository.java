bcdc2c362f88aea8d766e719653e47e37816436a
BlueWorldInc <ocalnuh@gmail.com>
2019-10-22 16:24:26 +0200

    [fix] Repository and Services. Server is Able to Run without Error

--- src/main/java/com/inti/formation/iRepositories/IGenericRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inti.formation.iRepositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IGenericRepository<G, I> extends JpaRepository<G, I> {
-
-}
