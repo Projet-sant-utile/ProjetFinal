c789a33e0b4eb02b871c09530440aad52f961559
Amanda MOMPO <amanda.mompo@outlook.fr>
2019-10-23 12:33:21 +0200

    [Upadte] Appointment with idLocation

--- src/main/java/com/inti/formation/entities/Appointment.java
+++ src/main/java/com/inti/formation/entities/Appointment.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -39,6 +41,10 @@ public class Appointment implements IHasId<Integer> {
 	@JoinColumn(name = "idHealthProfessional")
 	private HealthProfessional healthProfessional;
 
+	@ManyToOne
+	@JoinColumn(name = "idLocation")
+	private Location location;
+	
 	@Temporal(TemporalType.DATE)
 	private Date appointmentDate;
 	private int hourStart;
